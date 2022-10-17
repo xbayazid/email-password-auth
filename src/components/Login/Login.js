1e609542d0b1cf20b8d4f0e715562908ec78a6c3
xbayazid <bayazid9006@gmail.com>
2022-10-17 17:30:02 +0600

    fully responsiveness done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,82 +1,129 @@
-import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import app from "../../firebase/firebase.init";
-import loginImg from '../../login.webp';
+import loginImg from "../../login.webp";
 
 const auth = getAuth(app);
 
 const Login = () => {
-    const [success, setSuccess] = useState(false);
-    const [userEmail, setUserEmail] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [userEmail, setUserEmail] = useState("");
 
-    const handleSubmit = event => {
-        event.preventDefault();
-        setSuccess(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSuccess(false);
 
-        const form = event.target;
-        const email = form.email.value;
-        const password = form.password.value;
-        console.log(email, password)
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
 
-        signInWithEmailAndPassword(auth, email, password)
-            .then(result => {
-                const user = result.user;
-                console.log(user);
-                setSuccess(true);
-            })
-            .catch(error => {
-                console.error('error', error)
-            })
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
+      })
+      .catch((error) => {
+        console.error("error", error);
+      });
+  };
 
+  const handleEmailBlur = (event) => {
+    const email = event.target.value;
+    setUserEmail(email);
+    console.log(email);
+  };
+
+  const handleForgetPassword = () => {
+    if (!userEmail) {
+      alert("Please enter your email address.");
+      return;
     }
-
-    const handleEmailBlur = event => {
-        const email = event.target.value;
-        setUserEmail(email);
-        console.log(email);
-    }
-
-    const handleForgetPassword = () => {
-        if (!userEmail) {
-            alert('Please enter your email address.')
-            return;
-        }
-        sendPasswordResetEmail(auth, userEmail)
-            .then(() => {
-                alert('Password Reset email sent. Please check your email.')
-            })
-            .catch(error => {
-                console.error(error);
-            })
-    }
+    sendPasswordResetEmail(auth, userEmail)
+      .then(() => {
+        alert("Password Reset email sent. Please check your email.");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <div>
-        <Container className="d-flex mt-5">
-        <div className="w-50 mx-auto mt-5 bg register">
-      <form onSubmit={handleSubmit}>
-      <h3 className="text-danger fst-italic">Please Login!</h3>
-      <div className="mb-3 w-50 mt-4">
-        <label htmlFor="formGroupExampleInput" className="form-label">Email Address </label>
-        <input onBlur={handleEmailBlur} type="email" name="emil" className="form-control" id="formGroupExampleInput"
-          placeholder="Enter your email" required/>
-      </div>
-      <div className="mb-3 w-50">
-        <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
-        <input type="password" name="password" className="form-control" id="formGroupExampleInput2" placeholder="Password" required
-        />
-      </div>
-      <Button variant="danger" type="submit">Login</Button>
-      </form>
-      {success && <p>Successfully login to the account</p>}
-      <p className="mt-3 link"><small>New to this website? Please <Link to='/register'>Register</Link></small></p>
-      <p className="link"><small>Forget Password? <button type="button" onClick={handleForgetPassword} className="btn btn-link">Reset Password</button></small></p>
-    </div>
-    <div className="w-50">
-        <img className="img-fluid w-100" src={loginImg} alt="" />
-    </div>
-        </Container>
+      <Container className="d-flex mt-5">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="mx-auto mt-5 bg register">
+              <form onSubmit={handleSubmit}>
+                <h3 className="text-danger fst-italic">Please Login!</h3>
+                <div className="mb-3 w-50 mt-4">
+                  <label htmlFor="formGroupExampleInput" className="form-label">
+                    Email Address{" "}
+                  </label>
+                  <input
+                    onBlur={handleEmailBlur}
+                    type="email"
+                    name="emil"
+                    className="form-control"
+                    id="formGroupExampleInput"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </div>
+                <div className="mb-3 w-50">
+                  <label
+                    htmlFor="formGroupExampleInput2"
+                    className="form-label"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    id="formGroupExampleInput2"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <Button variant="danger" type="submit">
+                  Login
+                </Button>
+              </form>
+              {success && <p>Successfully login to the account</p>}
+              <p className="mt-3 link">
+                <small>
+                  New to this website? Please{" "}
+                  <Link to="/register">Register</Link>
+                </small>
+              </p>
+              <p className="link">
+                <small>
+                  Forget Password?{" "}
+                  <button
+                    type="button"
+                    onClick={handleForgetPassword}
+                    className="btn btn-link"
+                  >
+                    Reset Password
+                  </button>
+                </small>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div>
+              <img className="img-fluid w-100" src={loginImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,68 +3,92 @@ import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import app from "../../firebase/firebase.init";
-import './Register.css';
-import registerImg from '../../register.webp';
+import "./Register.css";
+import registerImg from "../../register.webp";
 
 const auth = getAuth(app);
 
 const Register = () => {
-    const [passwordError, setPasswordError] = useState('');
-    const [success, setSuccess] = useState(false);
-    const handleRegister = event =>{
-        event.preventDefault();
-        setSuccess(false);
-        const form = event.target;
-        const email= form.email.value;
-        const password = form.password.value;
-        console.log(email, password);
-        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
-          setPasswordError('Please provide at least two uppercase');
-          return
-        }
-        if(password.length < 6){
-          setPasswordError('Password should be at least 6 characters');
-          return;
-        }
-        setPasswordError('');
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result =>{
-          const user = result.user;
-          console.log(user);
-          setSuccess(true);
-          form.reset();
-        })
-        .catch(error =>{
-          console.log('error', error);
-          setPasswordError(error.message);
-        })
+  const [passwordError, setPasswordError] = useState("");
+  const [success, setSuccess] = useState(false);
+  const handleRegister = (event) => {
+    event.preventDefault();
+    setSuccess(false);
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setPasswordError("Please provide at least two uppercase");
+      return;
     }
+    if (password.length < 6) {
+      setPasswordError("Password should be at least 6 characters");
+      return;
+    }
+    setPasswordError("");
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setSuccess(true);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log("error", error);
+        setPasswordError(error.message);
+      });
+  };
 
   return (
     <div>
       <Container className="d-flex mt-5">
-      <div className="w-50 mx-auto mt-5 bg register">
-        <h3 className="text-danger mb-4 fst-italic">Please Register!</h3>
-      <Form onSubmit={handleRegister}>
-        <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter your email" required />
-        </Form.Group>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="mx-auto mt-5 bg register">
+              <h3 className="text-danger mb-4 fst-italic">Please Register!</h3>
+              <Form onSubmit={handleRegister}>
+                <Form.Group className="mb-3 w-50" controlId="formBasicEmail">
+                  <Form.Label>Email Address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    required
+                  />
+                </Form.Group>
 
-        <Form.Group className="mb-3 w-50" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" placeholder="Password" required />
-        </Form.Group>
-        <p className="text-danger">{passwordError}</p>
-        {success && <p className="text-success">User created Successfully</p>}
-        <Button variant="danger" type="submit">Register</Button>
-      </Form>
-      <p className="mt-3 link"><small>Already have an account? Please <Link to='/login'>Login</Link></small></p>
-    </div>
-    <div className="w-50">
-      <img className="img-fluid" src={registerImg} alt="" />
-    </div>
+                <Form.Group className="mb-3 w-50" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    required
+                  />
+                </Form.Group>
+                <p className="text-danger">{passwordError}</p>
+                {success && (
+                  <p className="text-success">User created Successfully</p>
+                )}
+                <Button variant="danger" type="submit">
+                  Register
+                </Button>
+              </Form>
+              <p className="mt-3 link">
+                <small>
+                  Already have an account? Please <Link to="/login">Login</Link>
+                </small>
+              </p>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div>
+              <img className="img-fluid" src={registerImg} alt="" />
+            </div>
+          </div>
+        </div>
       </Container>
     </div>
   );
