f54915c608aa7d9fac9d6100d41c2eb255e1e1f0
xbayazid <bayazid9006@gmail.com>
2022-10-17 13:16:09 +0600

    form required warning & password expression added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,34 @@
-import React from "react";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import app from "../../firebase/firebase.init";
+
+const auth = getAuth(app);
 
 const Register = () => {
+    const [passwordError, setPasswordError] = useState('');
     const handleRegister = event =>{
         event.preventDefault();
         const email= event.target.email.value;
         const password = event.target.password.value;
-        console.log(email, password)
+        console.log(email, password);
+        if(!/(?=.*[A-Z].*[A-Z])/.test(password)){
+          setPasswordError('Please provide at least two uppercase');
+          return
+        }
+        if(password.length < 6){
+          setPasswordError('Password should be at least 6 characters');
+          return;
+        }
+        
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result =>{
+          const user = result.user;
+          console.log(user);
+        })
+        .catch(error =>{
+          console.log('error', error);
+        })
     }
 
   return (
@@ -15,12 +37,12 @@ const Register = () => {
       <Form onSubmit={handleRegister}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" name="email" placeholder="Enter email" />
+          <Form.Control type="email" name="email" placeholder="Enter email" required />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" name="password" placeholder="Password" />
+          <Form.Control type="password" name="password" placeholder="Password" required />
         </Form.Group>
         <Button variant="danger" type="submit">Register</Button>
       </Form>
